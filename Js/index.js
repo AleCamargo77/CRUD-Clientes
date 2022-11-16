bbcbe401e310d5fc181942bc557c2107d184e292
Alessandro Camargo <acamargodias@terra.com.br>
2022-11-16 09:13:14 -0300

    ajustes finais do CRUD

--- Js/index.js
+++ Js/index.js
@@ -1,11 +1,11 @@
-// "use strict";
-
 const openModal = () =>
   document.getElementById("modal").classList.add("active");
 
 const closeModal = () => {
   clearFields();
   document.getElementById("modal").classList.remove("active");
+  const h2 = document.getElementById("client-h2");
+  h2.innerText = "Novo Cliente";
 };
 
 const getLocalStorage = () =>
@@ -42,16 +42,78 @@ const save = () => {
       cell: document.getElementById("cellphone").value,
       city: document.getElementById("city").value,
     };
-    createClient(client);
-    closeModal();
+    const index = document.getElementById("name").dataset.index;
+    if (index == "new") {
+      createClient(client);
+      updateTable();
+      closeModal();
+    } else {
+      updateClient(index, client);
+      updateTable();
+      closeModal();
+    }
   }
 };
 
-const createRow = () => {};
+const createRow = (client, index) => {
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+    <td>${client.name}</td>
+    <td>${client.niver}</td>
+    <td>${client.cell}</td>
+    <td>${client.city}</td>
+    <td>
+      <button type="button" class="button green" data-action="edit" id="edit-${index}">editar</button>
+     <button type="button" class="button red" data-action="delete" id="delete-${index}">excluir</button>
+    </td>
+  `;
+  const tbody = document.getElementById("tbodyClient");
+  tbody.appendChild(newRow);
+};
 
 const updateTable = () => {
   const dbClient = readClient();
+  clearTable();
   dbClient.forEach(createRow);
+};
+
+const editClient = (index) => {
+  const client = readClient()[index];
+  client.index = index;
+  fillFields(client);
+  const h2 = document.getElementById("client-h2");
+  h2.innerText = `Atualizando cliente`;
+  openModal();
+};
+
+const fillFields = (client) => {
+  document.getElementById("name").value = client.name;
+  document.getElementById("birthday").value = client.niver;
+  document.getElementById("cellphone").value = client.cell;
+  document.getElementById("city").value = client.city;
+  document.getElementById("name").dataset.index = client.index;
+};
+
+const editDelete = (ev) => {
+  if (ev.target.type == "button") {
+    const [action, index] = ev.target.id.split("-");
+    if (action == "edit") {
+      editClient(index);
+    } else {
+      const client = readClient()[index];
+      const responseDelete = confirm(
+        `Deseja realmente exluir o cliente ${client.name}?`
+      );
+      if (responseDelete) {
+        deleteClient(index);
+        updateTable();
+      }
+    }
+  }
+};
+const clearTable = () => {
+  const rows = document.querySelectorAll("#tbodyClient tr");
+  rows.forEach((row) => row.parentNode.removeChild(row));
 };
 
 const clearInputs = () => {
@@ -99,3 +161,9 @@ document.getElementById("registerClient").addEventListener("click", openModal);
 document.getElementById("modalClose").addEventListener("click", closeModal);
 
 document.getElementById("save").addEventListener("click", save);
+
+document
+  .querySelector("#tableClient > tbody")
+  .addEventListener("click", editDelete);
+
+updateTable();
